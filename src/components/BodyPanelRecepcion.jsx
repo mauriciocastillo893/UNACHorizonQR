7d904f3b6cfc3c0606533c542f65cfbfebcb7e86
CassDeLaRy <mauriciomtz893@gmail.com>
2023-04-14 17:13:05 -0600

    The next commit where I add Reception View and modifications from a new Header from the others views and the Footer Component

--- src/components/BodyPanelRecepcion.jsx
+++ src/components/BodyPanelRecepcion.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import '../style-sheets/BodyPanelRecepcion.css'
 
 function BodyPanelRecepcion() {
-    return (  
-        <main className="main">
-            
-        </main>
+
+    const fecha = new Date();
+
+    const secciones = [
+        { titulo: "ASISTENCIA VISITANTES", subtitulo: "BUSCAR A UN VISITANTE PARA SU ASISTENCIA", boton: "BUSCAR VISITANTE" },
+        { titulo: "BUSCAR USUARIOS", subtitulo: "BUSCAR USUARIOS DENTRO DEL CENTRO DE AUTOACCESO", boton: "BUSCAR USUARIO CAA" },
+        { titulo: "CORREGIR USUARIOS", subtitulo: "UNIR A USUARIOS IGUALES PERO MAL ESCRITOS", boton: "IR AL ACOPLADOR" },
+        { titulo: "ASISTENCIA A USUARIOS", subtitulo: "DAR ASISTENCIA A USUARIOS DE AUTOACCESO", boton: "DAR ASISTENCIA" },
+        { titulo: "SALIDA DE EMERGENCIA", subtitulo: "DAR SALIDA DE EMERGENCIA A TODOS", boton: "SALIDA DE EMERGENCIA" }
+        ];
+    
+    return (
+    <div className="main">
+        <div className="first-part">
+            <div className="titleS1">
+                <p>PANEL DE RECEPCIÓN</p>
+            </div>
+            <div className="date">
+                <p>{fecha.toDateString()}</p>
+            </div>
+            <div className="subtitleS1">
+                <p>Seleccione una sección para navegar</p>
+            </div>
+        </div>
+        <div className="second-part">
+        {secciones.map((seccion, index) => (
+            <div className="container-card" key={index}>
+                <div className="item-title">
+                    <p>{seccion.titulo}</p>
+                </div>
+                <div className="subtitlePR">
+                    <p>{seccion.subtitulo}</p>
+                </div>
+                <div className="botton-subtitle">
+                <button className={`botton-sub hoverable ${index === 4 ? "bs-exit " : ""}${index === 4 ? "bse-hover" : ""}`}>{seccion.boton}</button>
+                </div>
+            </div>
+        ))}
+        </div>
+    </div>
     );
 }
 
---
+++ src/components/Header2.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import '../style-sheets/Header2.css'
+
+function Header2() {
+    return (  
+        <header>
+            <div className="container-header-2">
+                <div className="first-partH-2">
+                    <div className="itemH-2">
+                        <p>C.A.A. RECEPCIÓN</p>
+                    </div>
+                    <div className="item2H-2">
+                        
+                    </div>
+                    <div className="item3H-2">
+                        
+                    </div>
+                    <div className="item4H-2">
+                        
+                    </div>
+                    <div className="item5H-2">
+                        <div className="item5H-21">
+                            
+                        </div>
+                        <div className="item5H-22">
+                            <div className="item5H-221">
+                                <p>ROQUE MAURICIO MARTINEZ CASTILLO</p>
+                            </div>
+                            <div className="item5H-cerrarsesion">
+                                <button className="botton-cs hoverable">CERRAR SESIÓN</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="second-partH-2">
+
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header2;
